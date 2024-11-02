ea8bd245481e723064d6788ec4310e4c7882167a
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-02 19:59:09 +0100

    Stickynote added to the index.

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Login from "./Login"
+import Stickynote from "./StickyNote"
 import "../css/Index.css"
 
 const Index = () =>{
@@ -9,6 +10,7 @@ const Index = () =>{
     <section className="index-container d_flex">
         <img src="/images/login-background.jpeg" alt="background-image login" />
         <section className="login-parent">
+    <Stickynote />
       <Login />
       </section>
     </section>

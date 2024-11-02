6ab82495bf42ffab49ad1778e64c2955ba8854b8
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-02 20:12:42 +0100

    Added username and password.

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -10,8 +10,8 @@ const Index = () =>{
     <section className="index-container d_flex">
         <img src="/images/login-background.jpeg" alt="background-image login" />
         <section className="login-parent">
-    <Stickynote />
       <Login />
+    <Stickynote />
       </section>
     </section>
     </>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Login.css";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const correctLogin = () =>{
+    if (email === "peter@lorem-academy.nl" && password === "ditiseensterkwachtwoord") {
+      alert("Inloggen geslaagd!");
+    } else {
+      alert("Inloggen mislukt. Controleer je e-mailadres en wachtwoord.");
+    }
+  }
+
+
   return (
     <>
       <section className="login-container c_flex">
@@ -10,9 +23,9 @@ const Login = () => {
         </h1>
         <h2>Teacher</h2>
         <h3>Login met je Lorem Academy e-mailadres</h3>
-        <input type="text" placeholder="Lorem Academy e-mailadress" />
-        <input type="password" name="" id="" placeholder="Wachtwoord" />
-        <button type="button">Login</button>
+        <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Lorem Academy e-mailadress" />
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} name="" id="" placeholder="Wachtwoord" />
+        <button type="button" onClick={correctLogin} >Login</button>
         <p>
           Log uit en sluit alle webbrowsers wanneer je klaar bent om ervoor te
           zorgen dat niemand anders toegang heeft tot je persoonlijke gegevens.

b62fbaedb4c36a35c1c24e9646cef2fc80e5300c
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-03 20:16:01 +0100

    Testing schedule component. Added dynamic grid.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './css/Animation.css';
 import Index from './components/Index';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Home from "./components/Home";
+import Schedule from './components/Schedule';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Index />} />
+    //     <Route path="/home" element={<Home />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <>
+    <Schedule />
+    </>
   );
 }
 
---
+++ src/components/Schedule.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Schedule() {
+  return (
+    <section className="grid-parent">
+    </section>
+  );
+}
+
+export default Schedule;

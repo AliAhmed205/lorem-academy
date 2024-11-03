cf2e83e396f387197ab15b76c4bd2d80600a0059
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-03 20:58:02 +0100

    Days and hours set in position.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import './css/Animation.css';
-import Index from './components/Index';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import Home from "./components/Home";
+// import Index from './components/Index';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+// import Home from "./components/Home";
 import Schedule from './components/Schedule';
 
 function App() {
--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,17 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import roosterData from "../Rooster.json";
+import "../css/Schedule.css"
 
 function Schedule() {
-  const [vakken, setVakken] = useState([]);
-  const [vakNaam, setvakNaam] = useState("");
-  const [vakDuur, setvakDuur] = useState("");
+  // const [vakken, setVakken] = useState([]);
+  // const [vakNaam, setvakNaam] = useState("");
+  // const [vakDuur, setvakDuur] = useState("");
 
-  useEffect(() => {
-    setVakken(roosterData.rooster);
-  }, []);
+  // useEffect(() => {
+  //   setVakken(roosterData.rooster);
+  // }, []);
 
   return (
     <section className="grid-parent">
+        <h3>08:00</h3>
+      <h3>09:00</h3>
+      <h3>10:00</h3>
+      <h3>11:00</h3>
+      <h3>12:00</h3>
+      <h3>13:00</h3>
+      <h3>14:00</h3>
+      <h3>15:00</h3>
+      <h3>16:00</h3>
+      <h3>17:00</h3>
+      <h3>18:00</h3>
+
+      <h3>maandag</h3>
+      <h3>dinsdag</h3>
+      <h3>woensdag</h3>
+      <h3>donderdag</h3>
+      <h3>vrijdag</h3>
+
     </section>
   );
 }

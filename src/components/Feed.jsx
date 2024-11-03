5bb7eaa2b20d0f01c14b86c0c8782e4310852604
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-03 16:39:15 +0100

    Added Posts data. Map function for the posts. Post's been turned dynamic.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import "../css/Feed.css"
-import Posts from './Posts'
-import UserPost from './UserPost'
+import React, { useState } from 'react';
+import "../css/Feed.css";
+import Posts from './Posts';
+import UserPost from './UserPost';
+import PostData from "../Post.json";
 
-const Feed = () =>{
-  return(
+const Feed = () => {
+  const [message] = useState(PostData); 
+
+  return (
     <>
-    <section className="feed-container c_flex">
+      <section className="feed-container c_flex">
         <UserPost />
-        <Posts />
-        <Posts />
-        <Posts />
-        <Posts />
-    </section>
+        {message.map((post) => (
+          <Posts
+            key={post.id} 
+            photoUrl={post.photoUrl}
+            firstname={post.firstname}
+            surname={post.surname}
+            date={post.date}
+            postPhoto={post.postPhoto}
+            postDescription={post.post_description}
+            likes={post.likes}
+            saved={post.saved}
+          />
+        ))}
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,56 +1,80 @@
-import React from "react";
+import React, { useState } from 'react';
 import "../css/Feed.css";
-import "../css/SideBar.css";
 
-const Posts = () => {
+const Posts = ({ 
+  photoUrl, 
+  firstname, 
+  surname, 
+  date, 
+  postPhoto, 
+  postDescription, 
+  likes: addLikes, 
+  saved 
+
+}) => {
+
+  const [liked, setLikes] = useState(addLikes)
+  const [geliket, setGeliket] = useState(false)
+
+  const likePost = () =>{
+    if (!geliket) {
+    setLikes(liked + 1)
+    setGeliket(true)
+    } else {
+      setLikes(liked - 1)
+      setGeliket(false)
+    }
+  }
+
   return (
     <>
-      <article className="feed-posts">
-        <div className="user-container e_flex">
-          <img src="/images/user-peter.webp" alt="Peter Baan profielfoto" />
-          <div className="username">
-            <h1>Peter Baan</h1>
-            <h2>21 september 2024 om 08:45</h2>
-          </div>
+    <article className="feed-posts">
+    <div className="post-container">
+      <div className="user-container e_flex">
+        <img src={photoUrl} alt={`profielfoto van ${firstname} ${surname}`} />
+        <div className="username">
+          <h1>{firstname} {surname}</h1>
+          <h2>{date}</h2>
+        </div>
+        <button>
+          <i className="fa-solid fa-ellipsis"></i>
+        </button>
+      </div>
+
+      <p className="post text">{postDescription}</p>
+      {postPhoto && <img src={postPhoto} alt="foto van de post" />}
+
+      <div className="post interaction">
+        <hr />
+        <div>
+
+          <button onClick={likePost} className={`${geliket ? "interacted" : ""}`} >
+            <span>{liked}</span>
+            <i className="fa-solid fa-thumbs-up"></i> Vind-ik-leuk
+          </button>
           <button>
-            {" "}
-            <i className="fa-solid fa-ellipsis"></i>{" "}
+            <span>{saved}</span>
+            <i className="fa-solid fa-bookmark"></i> Opslaan
           </button>
         </div>
-        <p className="post text">This is a post</p>
-        <img src="/images/studenten.webp" alt="studenten op de foto" />
-        <div className="post interaction">
-          <hr />
-          <div>
-            <button>
-              <span>0</span>
-              <i className="fa-solid fa-comment"></i> Reacties
-            </button>
-            <button>
-              <span>0</span>
-              <i className="fa-solid fa-thumbs-up"></i> Likes
-            </button>
-            <button>
-              <span>0</span>
-              <i className="fa-solid fa-bookmark"></i> Opslaan
-            </button>
-          </div>
-          <hr />
-        </div>
+        {/* <hr /> */}
+      </div>
+    </div>
+     {/* <article className="post-something c_flex">
+     <div className="d_flex">
+       <img src="/images/user-peter.webp" alt="Peter Baan profielfoto" />
+       <div className="reageer-container d_flex">
+         <input type="text" name="post-message" placeholder="Reageren" />
+         <button>
+           <i className="fa-solid fa-paper-plane"></i>
+         </button>
+       </div>
+     </div>
+   </article> */}
+   </article>
+ </>
 
-        <article className="post-something c_flex">
-          <div className="d_flex">
-            <img src="/images/user-peter.webp" alt="Peter Baan profielfoto" />
-            <div className="reageer-container d_flex">
-              <input type="text" name="post-message" placeholder="Reageren" />
-              <button>
-                <i class="fa-solid fa-paper-plane"></i>
-              </button>
-            </div>
-          </div>
-        </article>
-      </article>
-    </>
+    
   );
 };
 

1674402495fdf8a32f461fa2837ad69a4708c0a6
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-03 20:39:45 +0100

    Back-up base while testing out the function. JSON linked to schedule.

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import roosterData from "../Rooster.json";
 
 function Schedule() {
+  const [vakken, setVakken] = useState([]);
+  const [vakNaam, setvakNaam] = useState("");
+  const [vakDuur, setvakDuur] = useState("");
+
+  useEffect(() => {
+    setVakken(roosterData.rooster);
+  }, []);
+
   return (
     <section className="grid-parent">
     </section>

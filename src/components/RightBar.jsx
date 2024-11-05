82e2d0fbb39cacfe0a00deacebefdf65feb25fbd
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-05 07:11:18 +0100

    Right sidebar created and styled. Meeting.json intigrated and reversed Post list.

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,10 +1,18 @@
 import React   from "react";
 import meetingData from "../Meetings.json";
+import postData from "../Post.json";
 
 const RightBar = () => {
+  
+  const sortedPostData = postData
+  .slice()
+  .sort((a, b) => {
+    return b.firstname.localeCompare(a.firstname);
+  });
+
   return (
-    <section className="rightbar-container">
-      <div className="meeting-container">
+    <section className="rightbar-container c_flex">
+      <div className="meeting-container padding-1">
         <h2> <i className="fa fa-solid fa-headset"></i> Meetings</h2>
         {meetingData.meetings.map((meeting, index) =>( 
           <div className="fade-in" style={{
@@ -15,6 +23,16 @@ const RightBar = () => {
         <h4>{meeting.day}</h4>
         <h5>{meeting.duration}</h5>
         </div>
+      ))}
+      </div>
+      <div className="chat-container padding-1">
+        <h2> <i className="fa fa-regular fa-comment"></i> Chat</h2>
+        {sortedPostData.map((post) =>( 
+
+        <button type="button" className="user e_flex">
+          <img src={post.photoUrl} alt={post.firstname} />
+          <h5>{post.firstname} {post.surname}</h5>
+        </button>
       ))}
       </div>
     </section>
